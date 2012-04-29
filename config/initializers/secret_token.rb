513f7fdd7261461ca8e38fbd7e83e671603f6039
Ershad K <ershad92@gmail.com>
2012-04-30 00:16:34 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArocMblog::Application.config.secret_token = '745cf493b182a75633fd8cac6146941b722855cf74eb91c2fe37de074d45182f0ba6a8aecc9057b0efc1074ffa1da6b14591671cb320f8270b83f95638aa2423'
